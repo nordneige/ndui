5dea2e0ab58d87d1b7db9a3d5bc2055abfa7ab13
nordneige <wtycust@163.com>
2017-05-06 18:30:25 +0800

    1

---
+++ NduiApi/Api/UserHanders.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NordNeige;
+
+namespace NduiApi.Web.Api
+{
+    public class UserHanders : ServerBase
+    {
+
+        [AjaxBase(MethodDesc = "系统登录")]
+        public object Login(string username, string password, string code)
+        {
+            if (username == "admin" && password == "admin")
+            {
+                UsersLogin.SaveLogin(username, username);
+
+                UsersLogin.SetPermission(PermissionType.Administrator);
+                UsersLogin.SetPermission(PermissionType.Developer);
+                UsersLogin.SetPermission(PermissionType.System);
+                UsersLogin.SetPermission(PermissionType.Developer);
+                ResponseModel.url = nd.Site.Url;
+                return User.UserCode;
+            }
+            return null;
+        }
+    }
+}
